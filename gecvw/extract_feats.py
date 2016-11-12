1da21ef8f67648e7a1ec8cc771eab9a4a273a2ea
romang <romang@amu.edu.pl>
2016-11-12 15:29:25 +0100

    Create single script for feature extraction

--- gecvw/extract_feats.py
+++ gecvw/extract_feats.py
@@ -6,41 +6,59 @@ import argparse
 import linecache
 
 from csets.cset import CSetPair
-from csets.cword_reader import CWordReader
+from csets.cword_finder import CWordFinder
 from feats.feature_extractor import FeatureExtractor
+from csets.cword_reader import CWordReader
 
 from config import load_config
+from logger import log
 
 
 def main():
     args = parse_user_args()
     config = load_config(args.config)
 
-    reader = CWordReader(args.cwords)
-    extractor = FeatureExtractor(
-        CSetPair(config['source-cset'], config['target-cset']),
-        config['features'],
-        config['costs'])
+    csets = CSetPair(
+        args.source_cset or config['source-cset'],
+        args.target_cset or config['target-cset'])
 
-    for sid, cword in reader:
-        sentence = linecache.getline(args.input,
-                                     sid + 1).strip().split("\t")[0].split()
-        feat_str = extractor.extract_features(cword, sentence)
-        args.output.write(feat_str)
+    finder = CWordFinder(csets, args.train)
+    extractor = FeatureExtractor(csets, config['features'], config['costs'])
+    reader = CWordReader(args.cwords)
+
+    count = 0
+    for sid, line in enumerate(args.input):
+        for cword in finder.find_confusion_words(line):
+
+            sentence = line.split("\t", 1)[0].split()
+            feat_str = extractor.extract_features(cword, sentence)
+            args.output.write(feat_str)
+
+            reader.format(sid, cword)
+            count += 1
+
+    log.info("Found {} confusion words".format(count))
 
 
 def parse_user_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('cwords', nargs='?', type=argparse.FileType('r'),
-        default=sys.stdin, help="confusion words file")
+    parser.add_argument('input', nargs='?', type=argparse.FileType('r'),
+        default=sys.stdin, help="input sentences")
     parser.add_argument('output', nargs='?', type=argparse.FileType('r'),
-        default=sys.stdout, help="output file")
+        default=sys.stdout, help="output features")
 
-    parser.add_argument('-i', '--input', required=True,
-        help="input sentences")
+    parser.add_argument('-1', '--source-cset',
+        help="source confusion set")
+    parser.add_argument('-2', '--target-cset',
+        help="target confusion set")
+
+    parser.add_argument('-c', '--cwords', type=argparse.FileType('w'),
+        required=True, help="file to store found confusion words")
     parser.add_argument('-f', '--config', required=True,
         help="configuration file")
+    parser.add_argument('-t', '--train', action='store_true',
+        help="turn on train mode")
 
     return parser.parse_args()
 
